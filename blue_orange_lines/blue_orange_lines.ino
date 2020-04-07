1ea9e501e265392dd1d238deb900abcfc4ad40e8
Enzo Galletta <galletta.e@northeastern.edu>
2020-04-07 11:53:06 -0400

    add arduino

---
+++ blue_orange_lines/blue_orange_lines.ino
@@ -0,0 +1,51 @@
+#include <Shifty.h>
+
+Shifty reg_blue;
+Shifty reg_orange;
+
+const int NUM_BLUE_STATIONS = 12;
+const int NUM_ORANGE_STATIONS = 20;
+
+
+
+// setup the shift registers and begin serial transmission
+void setup() {
+  reg_blue.setBitCount(16);
+  reg_blue.setPins(11, 12, 8);
+  reg_orange.setBitCount(24);
+  reg_orange.setPins(3, 4, 2);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // wait for a string to be send via serial,
+  // in the form of "[b | o]***...*", where * is either a "1" or "0"
+  while(Serial.available() > 0 ){
+    String str = Serial.readString();
+    if (str.substring(0,1) == "b") { // blue line LEDs
+      // turn on or off the appropriate LEDs hooked up to the shift register
+      reg_blue.batchWriteBegin();
+      for (int i = 1; i <= NUM_BLUE_STATIONS; i++) {
+        if (str.substring(i, i+1) == "1") {
+          reg_blue.writeBit((i - 1), HIGH);
+        }
+        else if (str.substring(i, i+1) == "0") {
+          reg_blue.writeBit((i - 1), LOW);
+        }
+      }
+      reg_blue.batchWriteEnd();
+    }
+    else if (str.substring(0,1) == "o") { // orange line LEDs
+      reg_orange.batchWriteBegin();
+      for (int i = 1; i <= NUM_ORANGE_STATIONS; i++) {
+        if (str.substring(i, i+1) == "1") {
+          reg_orange.writeBit((i - 1), HIGH);
+        }
+        else if (str.substring(i, i+1) == "0") {
+          reg_orange.writeBit((i - 1), LOW);
+        }
+      }
+      reg_orange.batchWriteEnd();
+    }
+  }
+}
---
+++ redline/redline.ino
@@ -0,0 +1,36 @@
+#include <Shifty.h>
+
+Shifty reg_red;
+
+const int NUM_red_STATIONS = 22;
+
+
+
+// setup the shift registers and begin serial transmission
+void setup() {
+  reg_red.setBitCount(24);
+  reg_red.setPins(11, 12, 8);
+  
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // wait for a string to be send via serial,
+  // in the form of "r***...*", where * is either a "1" or "0"
+  while(Serial.available() > 0 ){
+    String str = Serial.readString();
+    if (str.substring(0,1) == "r") {
+      reg_red.batchWriteBegin();
+      for (int i = 1; i <= NUM_red_STATIONS; i++) {
+        if (str.substring(i, i+1) == "1") {
+          reg_red.writeBit((i - 1), HIGH);
+        }
+        else if (str.substring(i, i+1) == "0") {
+          reg_red.writeBit((i - 1), LOW);
+        }
+      }
+      reg_red.batchWriteEnd();
+    }
+  }
+}
