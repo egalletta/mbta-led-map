e145a663508749abd07665c602b5939094a40c4f
Enzo Galletta <galletta.e@northeastern.edu>
2020-04-18 17:15:45 +0100

    debugging

---
+++ mbta_controller.py
@@ -0,0 +1,112 @@
+import time
+
+import requests
+import serial
+
+import config
+import stops
+from utils.stops import get_stop_name
+
+MBTA_KEY = config.api_key
+
+def populate_stops(line):
+    if (line == "Blue"):
+        current_positions = [False] * 12
+        stop_dict = stops.blue
+    elif (line == "Orange"):
+        current_positions = [False] * 20
+        stop_dict = stops.orange
+    elif (line == "Red"):
+        current_positions = [False] * 22
+        stop_dict = stops.red
+    elif ("Green" in line):
+        current_positions = [False] * 75
+        stop_dict = stops.green
+    return current_positions, stop_dict
+
+
+def write_serial_message(message, serial_device):
+    serial_device.write(message.encode())
+
+
+def convert(list):
+    result = ""
+    for item in list:
+        if item is True:
+            result = result + "1"
+        else:
+            result = result + "0"
+    return result
+
+def get_vehicles(line, direction):
+    # api-endpoint
+    URL = "https://api-v3.mbta.com/vehicles?filter[route]=" + line + "&api_key=" + MBTA_KEY
+    vehicles = requests.get(url=URL).json()['data']
+    #populating stops
+    current_positions, stop_dict = populate_stops(line)
+    for vehicle in vehicles:
+        if (vehicle['attributes']['direction_id'] == direction):
+            try:
+                 print(get_stop_name(str(vehicle['relationships']['stop']['data']['id'])))
+                 current_positions[stop_dict[get_stop_name(str(vehicle['relationships']['stop']['data']['id']))]] = True
+            except TypeError:
+                 print(vehicle)
+
+    return convert(current_positions)
+
+
+def main():
+    # set up the boards with their respective serial ports
+    blue_lightboard = serial.Serial('/dev/ttyUSB0', '115200')
+    red_lightboard = serial.Serial('/dev/ttyUSB1', '115200')
+    green_lightboard = serial.Serial('/dev/ttyUSB2', '115200')
+    display = serial.Serial('/dev/ttyUSB3', '115200')
+    while True:
+        # get southbound trains
+        write_serial_message("Trains NORTHBOUND   Updating...", display)
+        blue_train_positions = get_vehicles(line="Blue", direction=0)
+        orange_train_positions = get_vehicles(line="Orange", direction=0)
+        red_train_positions = get_vehicles(line="Red", direction=0)
+        green_train_positions = get_vehicles(line="Green-E,Green-B,Green-C,Green-D", direction=0)
+        time.sleep(0.6)
+        write_serial_message(message="b" + blue_train_positions, serial_device=blue_lightboard)
+        time.sleep(0.6)
+        write_serial_message(message="r" + red_train_positions, serial_device=red_lightboard)
+        write_serial_message(message="g" + green_train_positions, serial_device=green_lightboard)
+        time.sleep(0.6)
+        write_serial_message(message="o" + orange_train_positions, serial_device=blue_lightboard)
+        write_serial_message("Trains SOUTHBOUND", display)
+        time.sleep(8)
+        # get northbound trains
+        write_serial_message("Trains SOUTHBOUND   Updating...", display)
+        blue_train_positions = get_vehicles(line="Blue", direction=1)
+        orange_train_positions = get_vehicles(line="Orange", direction=1)
+        red_train_positions = get_vehicles(line="Red", direction=1)
+        green_train_positions = get_vehicles(line="Green-E,Green-B,Green-C,Green-D", direction=1)
+        time.sleep(0.6)
+        write_serial_message(message="b" + blue_train_positions, serial_device=blue_lightboard)
+        time.sleep(.6)
+        write_serial_message(message="r" + red_train_positions, serial_device=red_lightboard)
+        write_serial_message(message="g" + green_train_positions, serial_device=green_lightboard)
+        time.sleep(.6)
+        write_serial_message(message="o" + orange_train_positions, serial_device=blue_lightboard)
+        write_serial_message("Trains NORTHBOUND", display)
+        time.sleep(8)
+
+
+def clear_lights(blue_lightboard, red_lightboard, green_lightboard):
+    write_serial_message(message="b" + "000000000000000000000000000000000000000000000000000000000000000000000000",
+                         serial_device=blue_lightboard)
+    time.sleep(0.6)
+    write_serial_message(message="r" + "000000000000000000000000000000000000000000000000000000000000000000000000",
+                         serial_device=red_lightboard)
+    write_serial_message(message="g" + "000000000000000000000000000000000000000000000000000000000000000000000000",
+                         serial_device=green_lightboard)
+    time.sleep(0.6)
+    write_serial_message(message="o" + "000000000000000000000000000000000000000000000000000000000000000000000000",
+                         serial_device=blue_lightboard)
+    time.sleep(0.6)
+
+
+if __name__ == '__main__':
+    main()
--- utils/stops.py
+++ utils/stops.py
@@ -1,27 +1,24 @@
 import json
+import pickle
 from os import path
 
 import requests
 
-import config
-
-
 def main():
-    MBTA_KEY = config.api_key
-    URL = "https://api-v3.mbta.com/stops?api_key=" + MBTA_KEY
+    URL = "https://api-v3.mbta.com/stops"
     stops = requests.get(URL).json()['data']
     stops_optimized = dict()
     for stop in stops:
         stops_optimized[stop['id']] = stop['attributes']['name']
-    with open("stops.json", "w") as outfile:
-        json.dump(stops_optimized, outfile)
+    with open("stops.pickle", "wb") as file:
+         pickle.dump(stops_optimized, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 def get_stop_name(id):
-    if not path.exists("stops.json"):
+    if not path.exists("stops.pickle"):
         main()
-    with open("stops.json", "r") as stops_json:
-        stops = json.load(stops_json)
-        return stops[id]
+    with open("stops.pickle", "rb") as file:
+        stops = pickle.load(file)
+        return stops.get(id)
 
 if __name__ == '__main__':
     main()
